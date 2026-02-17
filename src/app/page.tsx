c9e7f43d9ef6c8d37a3c12c710a015bb02e21aea
poonam <poonamp@mindfiresolutions.com>
2026-02-17 19:15:32 +0530

    animation modifie

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,19 @@
 import Hero from "../components/Hero";
-import Navbar from "../components/Navbar";
 import ProductFeatureSection from "../components/ProductFeatureSection";
+import ProductGrid from "../components/ProductGrid";
+import AboutPage from "./about/page";
+import ContactPage from "./contact/page";
+import JournalPage from "./journal/page";
 
 export default function Home() {
   return (
     <main className="min-h-screen bg-white">
       <Hero/>
       <ProductFeatureSection />
+      <ProductGrid/>
+      <AboutPage/>
+      <JournalPage/>
+      <ContactPage/>
     </main>
   )
 }
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,106 +1,156 @@
 "use client"
 
 import { motion } from "framer-motion"
-import Image from "next/image"
+const capVariants = {
+  closed: { y: -50 },
+  open: {
+    y: -160,
+    transition: { duration: 1.5, ease: "easeOut" }
+  }
+}
+
+const bodyVariants = {
+  closed: { y: -160 },
+  open: {
+    y: 80,
+    transition: { duration: 1.5, ease: "easeOut" }
+  }
+}
+
+const sideBottleLeftPop = {
+  hidden: {
+    opacity: 0,
+    scale: 0.3,
+    rotate: -15,   // tilted left
+  },
+  show: {
+    opacity: 1,
+    scale: 1,
+    rotate: -15,   // stays tilted
+    transition: {
+      duration: 1.2,
+      ease: "easeOut"
+    }
+  }
+}
+
+const sideBottleRightPop = {
+  hidden: {
+    opacity: 0,
+    scale: 0.3,
+    rotate: 15,
+  },
+  show: {
+    opacity: 1,
+    scale: 1,
+    rotate: 15,
+    transition: {
+      duration: 1.2,
+      ease: "easeOut",
+    }
+  }
+}
 
 export default function Hero() {
   return (
-    <section className="relative flex flex-col items-center justify-center text-center overflow-hidden">
-      {/* Small Floating Bottles (Left) */}
+    <section className="relative min-h-screen flex flex-col items-center 
+                    pt-20 sm:pt-24 md:pt-32 
+                    text-center overflow-hidden">
+      <motion.img
+        src="/images/bottle-cap.png"
+        className="absolute top-32 left-1/2 -translate-x-1/2 z-30 w-62"
+        variants={capVariants}
+        initial="closed"
+        animate="open"
+      />
+      <div className="absolute left-40 bottom-40">
+        <motion.div
+          initial={{ scale: 0.5, opacity: 0 }}
+          animate={{ scale: 0.8, opacity: 1 }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+          className="w-48 h-48 rounded-full bg-gradient-to-r from-cyan-400 to-blue-600"
+        />
+
+        <motion.img
+          src="/images/borosil.png"
+          className="absolute inset-0 m-auto w-30"
+          variants={sideBottleLeftPop}
+          initial="hidden"
+          animate="show"
+        />
+      </div>
+
+      {/* Right Gradient Circle */}
+      <div className="absolute right-40 bottom-40">
+        {/* Gradient Circle */}
+        <motion.div
+          initial={{ scale: 0.5, opacity: 0 }}
+          animate={{ scale: 0.8, opacity: 1 }}
+          transition={{ duration: 1.5, ease: "easeOut" }}
+          className="w-48 h-48 
+                    rounded-full 
+                    bg-gradient-to-r from-cyan-400 to-blue-600
+                    z-0"
+        />
+
+        {/* Bottle */}
+        <motion.img
+          src="/images/borosil.png"
+          className="absolute inset-0 m-auto w-30 z-10"
+          variants={sideBottleRightPop}
+          initial="hidden"
+          animate="show"
+        />
+
+      </div>
+
+      {/* Circle */}
       <motion.div
-        initial={{ rotate: 12, opacity: 0 }}
-        animate={{ y: [0, -20, 0], opacity: 1 }}
-        transition={{ duration: 4, repeat: Infinity, ease: "easeInOut", opacity: { duration: 1 } }}
-        className="absolute left-30 top-1/2 -translate-y-1/2 w-[160px] h-[160px]"
+        initial={{ scale: 0.5 }}
+        animate={{ scale: 0.8 }}
+        transition={{ duration: 1.5, ease: "easeOut" }}
+        className="absolute top-0 left-1/2 -translate-x-1/2 z-0
+               w-64 h-64 sm:w-80 sm:h-80 md:w-lg md:h-128
+               rounded-full 
+               bg-linear-to-r from-cyan-500 to-blue-500 p-1
+               flex items-center justify-center"
       >
-        <div className="absolute inset-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
-        <div className="relative z-10 flex items-center justify-center h-full">
-          <Image
-            src="/images/bottle.png"
-            alt="Small Bottle"
-            width={50}
-            height={100}
-          />
-        </div>
+        <div className="w-full h-full rounded-full bg-white"></div>
       </motion.div>
 
-      {/* Small Floating Bottles (Right) */}
+      {/* Content */}
       <motion.div
-        initial={{ rotate: -12, opacity: 0 }}
-        animate={{ y: [0, 20, 0], opacity: 1 }}
-        transition={{ duration: 4, repeat: Infinity, ease: "easeInOut", opacity: { duration: 1 }  }}
-        className="absolute right-30 top-1/2 -translate-y-1/2 w-[160px] h-[160px]"
+        initial={{ opacity: 0, scale: 0.5 }}
+        animate={{ opacity: 1, scale: 1 }}
+        transition={{ duration: 1, delay: 0.6, ease: "easeOut" }}
+        className="relative z-10 max-w-3xl px-6"
       >
-        {/* Circle Background */}
-        <div className="absolute inset-0 rounded-full bg-gradient-to-r from-cyan-500 to-blue-500" />
+        <h1 className="text-3xl sm:text-4xl md:text-5xl font-bold leading-tight">
+          The Ultimate Companion <br /> for Hydration
+        </h1>
 
-        {/* Bottle Image */}
-        <div className="relative z-10 flex items-center justify-center h-full">
-          <Image
-            src="/images/bottle.png"
-            alt="Small Bottle"
-            width={50}
-            height={100}
-          />
-        </div>
+        <p className="mt-6 text-gray-700 text-sm sm:text-base">
+          We believe in the power of hydration.
+        </p>
+
+        <p className="text-gray-700 text-sm sm:text-base">
+          Our mission is simple yet vital.
+        </p>
+
+        <button className="mt-8 bg-linear-to-r from-cyan-500 to-blue-500 
+                       text-white px-6 py-3 rounded-full shadow-lg 
+                       hover:scale-105 transition">
+          Inquiry Now
+        </button>
       </motion.div>
-      <div className="relative z-10 flex flex-col items-center">
-        {/* Cap Floating */}
-        <motion.div
-          initial={{ y: 230, opacity: 0 }}
-          animate={{ y: 0, opacity: 1 }}
-          transition={{ duration: 1.5, delay: 0.4 }}
-        >
-          <Image
-            src="/images/bottle-cap.png"
-            alt="Bottle Cap"
-            width={220}
-            height={280}
-          />
-        </motion.div>
+      <motion.img
+        src="/images/bottle-body.png"
+        className="absolute bottom-0 left-1/2 -translate-x-1/2 z-20 w-100"
+        variants={bodyVariants}
+        initial="closed"
+        animate="open"
+      />
 
-        {/* Main Content */}
-        <motion.div
-          initial={{ opacity: 0, y: 80 }}
-          animate={{ opacity: 1, y: 0 }}
-          transition={{ duration: 1, delay: 1 }}
-          className="relative z-10 max-w-2xl"
-        >
-          <h1 className="text-5xl font-bold leading-tight">
-            The Ultimate Companion <br /> for Hydration
-          </h1>
-
-          <p className="mt-6 text-gray-600">
-            We believe in the power of hydration.
-            Our mission is simple yet vital.
-          </p>
-
-          <button className="mt-8 bg-gradient-to-r from-cyan-500 to-blue-500 text-white px-6 py-3 rounded-full shadow-lg hover:scale-105 transition">
-            Inquiry Now
-          </button>
-        </motion.div>
-
-        {/* Main Bottle */}
-        <motion.div
-          initial={{ y: -230, opacity: 0 }}
-          animate={{ y: -50, opacity: 1 }}
-          transition={{
-            duration: 1.5,
-            delay: 0.4,
-            // repeat: Infinity,
-            // ease: "easeInOut",
-          }}
-          className="relative mt-16"
-        >
-          <Image
-            src="/images/bottle-body.png"
-            alt="Bottle"
-            width={350}
-            height={350}
-            className="object-contain"
-          />
-        </motion.div>
-        </div>
     </section>
   )
 }
